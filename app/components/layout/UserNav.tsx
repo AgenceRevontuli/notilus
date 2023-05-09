9cc55f2f24b24fccd55dc25548f0ed31db42cfda
AgenceRevontuli <tristan@revontuli.fr>
2023-05-09 23:47:54 +0200

    Add Next config img domain-Profile pic from Google

--- app/components/layout/UserNav.tsx
+++ app/components/layout/UserNav.tsx
@@ -1,16 +1,26 @@
 'use client'
 import { LoginButton, LogoutButton } from '../auth/authButtons';
 import { useSession } from 'next-auth/react'
+import { ProfilePicture } from '../profile/profile';
+import Link from 'next/link'
 
 export default function UserNav() {
     const { data:session } = useSession()
   return (
     <li>
         {session? (
-            <LogoutButton />
+            <div className="flex flex-row gap-5 items-center">
+              <Link href="/notes">
+                Mes notes
+              </Link>
+              <ProfilePicture />
+              <LogoutButton />
+            </div>
         )
         : (
-            <LoginButton />
+            <div>
+              <LoginButton />
+            </div>
         )}
     </li>
   )
